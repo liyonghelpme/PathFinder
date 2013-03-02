29386d144ea38f50d6b01714971ebe7cb6b5deb6
liyong <liyonghelpme@gmail.com>
2013-03-02 11:23:22 +0800

    添加光线注释

--- LuaPath/Ray.lua
+++ LuaPath/Ray.lua
@@ -1,3 +1,7 @@
+--参考实现  http://dev.mothteeth.com/2011/11/2d-ray-casting-on-a-grid-in-as3/
+--设定起始点 和 终点 判断两点之间是否存在墙体
+--x y dx dy p0 p1 tar0 tar1 
+--在网格地图上每个点沿着射线方向第一个交点必然和 最近的X或者 Y轴相交，只需要判断是和X 先相交 还是和Y 先相交 
 require "Class"
 Ray = class()
 function Ray:ctor(a, b, world)
@@ -7,11 +11,13 @@ function Ray:ctor(a, b, world)
 end
 --  返回相交的所有网格
 function Ray:checkCollision()
+    --保存该光线检测过的网格
     self.checkedGrid = {}
-    local x, y = self.a[1], self.a[2]
-    local vector = {self.b[1]-self.a[1], self.b[2]-self.a[2]}
+    local x, y = self.a[1], self.a[2] --当前的点所在网格
+    local vector = {self.b[1]-self.a[1], self.b[2]-self.a[2]}--运动方向
     local dx = 0
     local dy = 0
+    --根据运动方向计算下一个移动网格的偏移值
     if vector[1] > 0 then
         dx = 1
     elseif vector[1] < 0 then
@@ -23,9 +29,11 @@ function Ray:checkCollision()
         dy = -1
     end
 
+    --操作对象当前的位置
     local p0 = x*self.world.cellSize+self.world.cellSize/2
     local p1 = y*self.world.cellSize+self.world.cellSize/2
 
+    --操作对象可能的下一个位置
     local tar0 = x
     if dx > 0 then
         tar0 = tar0 + dx
@@ -40,6 +48,7 @@ function Ray:checkCollision()
 
     table.insert(self.checkedGrid, {x, y})
     self.count = 0
+
 
     while (x ~= self.b[1] or y ~= self.b[2]) and self.count < 2*(math.abs(vector[1])+math.abs(vector[2]))  do
         self.count = self.count + 1
@@ -64,6 +73,7 @@ function Ray:checkCollision()
         print("time", t0, t1)
 
         
+        --根据运动时间较小的得到最近的相邻网格
         -- Y 方向先相交
         if t0 > t1 then
             x = x
